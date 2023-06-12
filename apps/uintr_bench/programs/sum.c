716377f01329c46fe843229b4543bf93a776a69a
linsongguo <l1guo@ucsd.edu>
2023-06-12 05:50:17 +0000

    add some microbench results

--- apps/uintr_bench/programs/sum.c
+++ apps/uintr_bench/programs/sum.c
@@ -3,32 +3,13 @@
 
 #include <x86intrin.h>
 
-const int N = 4e4;
-const int M = 10007;
-
-int *init() {
-	_clui();
-	int *a = malloc(N * sizeof(int));
-	_stui();
-
-	int i = 0, c = 0;
-	for (i = 0; i < N; ++i) {
-		c = (c + 1LL*i*i) % M;
-		a[i] = c;
-	}
-	return a; 
-}
-
+const int N = 5e4;
 long long sum() {
-	//_clui();
-	int *a = init();
-	//_stui();
-
 	long long res = 0;
 	int i, j;
 	for (i = 0; i < N; ++i) {
 		for (j = N - 1; j >= 0; --j) {
-			res += a[i] | a[j];
+			res += i | j;
 		}
 	}
 	// _clui();
---
+++ apps/uintr_bench/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+work=$1
+Tus=(100000000 10000 1000 100 50 20 10 5 4 3 2 1)
+Tname=(baseline 10ms 1ms 100us 50us 20us 10us 5us 4us 3us 2us 1us)
+trial=5
+
+folder="results/$work"
+if [ ! -d "$folder" ]; then
+	mkdir $folder
+fi 
+
+for ((i=0; i<${#Tus[@]}; i++)); do 
+    tus=${Tus[i]}
+    tname=${Tname[i]}
+	echo $tname
+
+	folder="results/$work/$tname"
+	if [ ! -d "$folder" ]; then
+		mkdir $folder
+    fi 
+       
+	export INT_INTERVAL=$tus 
+	for ((j=1; j<=trial; j++)); do
+        echo trial $j
+		result="$folder/$j"
+        ./bench ../../server.config 1 1 $work >$result 
+    done
+done
